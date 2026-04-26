ab3a9539a3545ff122b48dae4d7b661d77916ded
Shivansh Soni <s.soni.shivansh@gmail.com>
2026-04-26 11:00:26 +0530

    Understanding the Response Object

---
+++ lesson_01b.py
@@ -0,0 +1,27 @@
+# lesson_01b.py — Understanding AIMessage
+
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import HumanMessage, SystemMessage
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-3-flash-preview",
+    temperature=0
+)
+
+
+messages = [
+    SystemMessage(content="You are a Python tutor. Always use simple examples."),
+    HumanMessage(content="What is a list in Python?")
+]
+
+#  Or 
+# response = llm.invoke([
+#     ("system", "You are a friendly assistant named Jarvis."),
+#     ("human", "What's your name and what can you do?")
+# ])
+
+response = llm.invoke(messages)
+print(response.content)
